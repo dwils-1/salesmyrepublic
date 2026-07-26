3eaabf32d9d7af96a593825c6eabe2187af73ccb
dwils-1 <EMAIL_GITHUB_KAMU>
2026-07-26 21:06:54 +0700

    SalesMyRepublic v3 Production

--- build/build.js
+++ build/build.js
@@ -1,18 +1,12 @@
 const {execSync}=require("child_process");
 
-const jobs=[
+[
 "node build/generate.js",
 "node build/articles.js",
 "node build/faq.js",
 "node build/promo.js",
 "node build/sitemap.js"
-];
-
-jobs.forEach(j=>{
-console.log("\n======================");
-console.log(j);
-console.log("======================");
-execSync(j,{stdio:"inherit"});
+].forEach(c=>{
+console.log(">>",c);
+execSync(c,{stdio:"inherit"});
 });
-
-console.log("\nBUILD SELESAI");
